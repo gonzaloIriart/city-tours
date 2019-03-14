e337b264b72c60d17c56ddc8e26fd080ef42a2d2
GonzaloIriart <gonzaloiriart@gmail.com>
2019-03-14 16:52:16 -0300

    added navbar and icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import NavBar from './components/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
         <h1>City tours</h1>
       </div>
     );
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './NavBar.scss';
+import logo from '../../images/logo.png';
+
+export default function NavBar() {
+  return (
+    <nav className = "navbar">
+        <img src = {logo} alt = "company logo" className = "logo"/>
+        <ul className = "nav-links">
+        <li>
+            <a href = "/" className = "nav-link">Home</a>
+        </li>
+        <li>
+            <a href = "/" className = "nav-link">About</a>
+        </li>
+        <li>
+            <a href = "/" className = "nav-link active">Tours</a>
+        </li>
+        </ul>
+    </nav>
+  )
+}
